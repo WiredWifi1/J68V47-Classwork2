b8816f97402f2955d58f315365a10ab24b99fc93
WiredWifi1 <30130302@nescol.ac.uk>
2023-09-19 11:44:03 +0100

    New attempt at all of this

--- src/L1E1.java
+++ src/L1E1.java
@@ -2,6 +2,6 @@ public class L1E1 {
     public static void main (String[] args) {
         System.out.println("Heat wave scorches land") ;
         System.out.println("Crops wilt under blazing sun") ;
-        System.out.println("Climate change marches on") ;
+        System.out.println("Climate change marches on.") ;
     }
 }
