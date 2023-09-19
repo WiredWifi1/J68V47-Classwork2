33e1c95e7e768a70b57a3a758c4e8e3c5ce2f9a2
WiredWifi1 <30130302@nescol.ac.uk>
2023-09-19 12:01:46 +0100

    New attempt at all of this

---
+++ src/L1E2.java
@@ -0,0 +1,10 @@
+public class L1E2 {
+    public static void main(String[] args) {
+        System.out.println("Here are 5 9-Lettered Anagrams:");
+        System.out.println("Aced Qi Tut - Acquitted");
+        System.out.println("Hod Tyro Ox - Orthodoxy");
+        System.out.println("Drape Lazy - Paralyzed");
+        System.out.println("Cog Ire Zen - Recognize");
+        System.out.println("Nudge Plug - Unplugged");
+    }
+}
