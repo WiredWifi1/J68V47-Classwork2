e30b29a3ff34a36b72cd0f02f7244838ddc4711a
WiredWifi1 <30130302@nescol.ac.uk>
2023-10-25 17:10:13 +0100

    Taking input from user, outputting a username and password based on input data.

---
+++ src/L3E2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class L3E2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the capital of Spain?");
+        String Spain = input.nextLine();
+        System.out.println(Spain.contains("Madrid"));
+        System.out.println("What is the capital of the UK? ");
+        String UK = input.nextLine();
+        System.out.println(UK.contains("London"));
+        System.out.println("What is the capital of Italy? ");
+        String Italy = input.nextLine();
+        System.out.println(Italy.contains("Rome"));
+    }
+}
